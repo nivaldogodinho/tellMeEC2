93be5839d3ab1bd5441d7e70ff0af3a56a6a72e7
moav2 <magno.alves@protonmail.com>
2021-08-23 14:48:43 -0300

    Implementações - Instancia ec2 - Description

--- ec2_Describe_Instance.py
+++ ec2_Describe_Instance.py
@@ -5,7 +5,8 @@ client = boto3.client('ec2',region_name='us-east-1')
 
 response = client.describe_instances()
 
-#Informações sobre a Instancia ec2
+#Informações sobre a Instancia ec2#
+
 for info in response['Reservations']:
     for instance in info['Instances']:
         instance_id = instance ['InstanceId']
@@ -19,7 +20,9 @@ for info in response['Reservations']:
         instance_security_name = instance ['SecurityGroups'] [0]['GroupName']
         print(f'{instance_id} - {instance_type} - {instance_state_name} - {instance_priv_ipv4} - {instance_availability_zone} - {instance_security_name} - {instance_subnetid} - {instance_vpcid} - {instance_priv_ipv4}')
 
-#Informações do Security Group
+
+#Informações do Security Group #  
+
         response=client.describe_security_groups()
 print(f'{response}')
         
